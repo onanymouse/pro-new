c07bf3833121c10e2d0d335f37b4a65a9cfb81ef
onanymouse <50264820+onanymouse@users.noreply.github.com>
2025-11-21 02:01:27 +0700

    Refactor Model to use Database singleton

--- app/core/Model.php
+++ app/core/Model.php
@@ -1,24 +1,11 @@
 <?php
-// app/core/Model.php
+
 class Model
 {
     protected $db;
 
     public function __construct()
     {
-        // load config DB
-        $cfg = __DIR__ . '/../config/database.php';
-        if (!file_exists($cfg)) {
-            throw new \Exception("Database config not found: app/config/database.php");
-        }
-        require $cfg;
-
-        // gunakan PDO
-        $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4";
-        $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        ];
-        $this->db = new PDO($dsn, DB_USER, DB_PASS, $options);
+        $this->db = Database::getInstance();
     }
 }
