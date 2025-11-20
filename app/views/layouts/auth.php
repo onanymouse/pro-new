decf14ef72f4f0f86253190805b77641a709dd27
onanymouse <50264820+onanymouse@users.noreply.github.com>
2025-11-21 01:35:23 +0700

    Update HTML structure and meta tags in auth layout

--- app/views/layouts/auth.php
+++ app/views/layouts/auth.php
@@ -1,20 +1,21 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title><?= $title ?? 'Login Billing ISP' ?></title>
-    <!-- AdminLTE CSS -->
-    <link rel="stylesheet" href="/assets/vendor/adminlte/dist/css/adminlte.min.css">
-    <!-- FontAwesome CDN -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-    <!-- jQuery CDN -->
-    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title><?= $title ?? 'Login' ?></title>
+
+<!-- AdminLTE (local dist) -->
+<link rel="stylesheet" href="/assets/vendor/adminlte/dist/css/adminlte.min.css">
+
+<!-- FontAwesome CDN -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+<!-- jQuery CDN (AdminLTE 4 may not require jQuery for core, but some plugins use it) -->
+<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
 </head>
 <body class="hold-transition login-page">
-
 <?= $content ?? '' ?>
-
-<!-- AdminLTE JS -->
 <script src="/assets/vendor/adminlte/dist/js/adminlte.min.js"></script>
 </body>
 </html>
